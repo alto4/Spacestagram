d1b65e8d481de1ed98cd208617556c1871c1c627
Scott Alton <scottaltonmusic@gmail.com>
2022-01-08 14:36:53 -0500

    Add react boilerplate, client dependencies, cleanup cra files.

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import { Fragment } from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <Fragment>
+      <h1>
+        Spacestagram <i className='fa fa-earth'></i>
+      </h1>
+    </Fragment>
+  );
+};
+
+export default App;
